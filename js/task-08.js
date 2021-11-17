61d5fab5f204484334f3a90206878d26cf2d3028
Roman-Pashkevych <pashkevych_roman@ukr.net>
2021-11-17 23:33:40 +0200

    task-8-done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+console.log(form);
 
+const onSubmit = (event) => {
+    event.preventDefault();
+
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (!email || !password) return alert('Всі поля мають бути заповнені!');
+        
+    console.log({
+        email,
+        password,
+    });
+    form.reset();
+}
+
+form.addEventListener('submit', onSubmit);
