b667305baac2daa2c87b35601464f8f934f5cd18
Roman-Pashkevych <pashkevych_roman@ukr.net>
2021-11-18 00:36:59 +0200

    hw-06-finish

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const body = document.body;
+const backgroundColor = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const handleChangeColor = () => {
+  const currentColor = getRandomHexColor();
+  body.setAttribute('style', `background:${currentColor}`);
+  backgroundColor.textContent = currentColor;
+};
+
+changeColorBtn.addEventListener('click', handleChangeColor);
